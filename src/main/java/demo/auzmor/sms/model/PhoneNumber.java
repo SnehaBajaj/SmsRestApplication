69e64ec469b8368e47bcc7c1d8b3cbcb8296ea54
Sneha  Bajaj <snehabajaj255@gmail.com>
2019-05-11 22:01:20 +0530

    Introducing basic auth, phone number model

---
+++ src/main/java/demo/auzmor/sms/model/PhoneNumber.java
@@ -0,0 +1,19 @@
+package demo.auzmor.sms.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="phone_number")
+@Getter
+public class PhoneNumber {
+
+    @Id
+    @GeneratedValue(generator="account_id_generator")
+    @SequenceGenerator(
+            name = "account_id_generator",
+            sequenceName = "account_id_sequence"
+    )
+    private Integer id;
+}
